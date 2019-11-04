b2ceccda21aa6100cb8c1b806cb5cdeb014a2180
Lewis Hazell <lewis.hazell@hotmail.co.uk>
2019-11-04 21:49:18 +0100

    Separated inverted tests to allow state to be re-initialised. Added date/pagination test.

--- MOT.NET.Tests/MOTRequestBuilderParameterTests.cs
+++ MOT.NET.Tests/MOTRequestBuilderParameterTests.cs
@@ -5,22 +5,27 @@ using System.Security;
 
 namespace MOT.NET.Tests {
     public class MOTRequestBuilderParameterTests {
-        private Core Setup() {
-            return new Core(new SecureString());
-        }
+        private Core _core = new Core(new SecureString());
 
         [Fact]
-        public void Registration_Query_Cannot_Have_Date_Parameter() {
-            Core core = Setup();
-            Assert.Throws<InvalidParametersException>(() => core.MOTs().Registration("F1").Date(DateTime.Today));
-            Assert.Throws<InvalidParametersException>(() => core.MOTs().Date(DateTime.Today).Registration("F1"));
-        }
+        public void Registation_Query_Cannot_Specify_Date()
+            => Assert.Throws<InvalidParametersException>(() => _core.MOTs().Registration("F1").Date(DateTime.Today));
 
         [Fact]
-        public void Registration_Query_Cannot_Be_Paginated() {
-            Core core = Setup();
-            Assert.Throws<InvalidParametersException>(() => core.MOTs().Registration("F1").Page(1));
-            Assert.Throws<InvalidParametersException>(() => core.MOTs().Page(1).Registration("F1"));
-        }
+        public void Date_Query_Cannot_Specify_Registration()
+            => Assert.Throws<InvalidParametersException>(() => _core.MOTs().Registration("F1").Date(DateTime.Today));
+
+        [Fact]
+        public void Registration_Query_Cannot_Specify_Page()
+            => Assert.Throws<InvalidParametersException>(() => _core.MOTs().Registration("F1").Page(1));
+        
+
+        [Fact]
+        public void Page_Query_Cannot_Specify_Registration()
+            => Assert.Throws<InvalidParametersException>(() => _core.MOTs().Page(1).Registration("F1"));
+
+        [Fact]
+        public void Date_Query_Can_Be_Paginated()
+            => _core.MOTs().Date(DateTime.Today).Page(1);
     }
 }
