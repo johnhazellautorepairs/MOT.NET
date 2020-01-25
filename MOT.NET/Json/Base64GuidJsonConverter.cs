85648f918182d74ee9c606796280d4e604f593e5
Lewis Hazell <lewis.hazell@hotmail.co.uk>
2020-01-25 20:42:49 +0000

    Added Base64GuidJsonConverter. Supports reading vehicle identifiers.

---
+++ MOT.NET/Json/Base64GuidJsonConverter.cs
@@ -0,0 +1,20 @@
+using System;
+using Newtonsoft.Json;
+
+namespace MOT.NET.Json {
+    public class Base64GuidJsonConverter : JsonConverter<Guid> {
+        public override Guid ReadJson(JsonReader reader, Type objectType, Guid existingValue, bool hasExistingValue, JsonSerializer serializer) {
+            string base64 = reader.Value.ToString().Replace('_', '/');
+            byte[] raw = Convert.FromBase64String(base64);
+            Guid guid = new Guid(raw);
+            return guid;
+        }
+
+        public override void WriteJson(JsonWriter writer, Guid value, JsonSerializer serializer) {
+            byte[] raw = value.ToByteArray();
+            string base64 = Convert.ToBase64String(raw);
+            string transposed = base64.Replace('/', '_');
+            writer.WriteValue(transposed);
+        }
+    }
+}
