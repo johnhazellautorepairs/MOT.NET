fe55b6fecc7af34ba1e43016a32b95a8d81961fb
Lewis Hazell <lewis.hazell@hotmail.co.uk>
2020-02-02 12:08:47 +0000

    Documented InvalidParametersException.

--- MOT.NET/InvalidParametersException.cs
+++ MOT.NET/InvalidParametersException.cs
@@ -2,14 +2,30 @@ using System;
 using System.Runtime.Serialization;
 
 namespace MOT.NET {
+    /// <summary>
+    /// An exception thrown when an invalid set of parameters has been specified.
+    /// </summary>
     public class InvalidParametersException : MOTNETException
     {
+        /// <summary>
+        /// Constructs a new InvalidParametersException.
+        /// </summary>
         public InvalidParametersException() {
         }
 
+        /// <summary>
+        /// Constructs a new InvalidParametersException with a descriptive message.
+        /// </summary>
+        /// <param name="message">A message descibing the Exception.</param>
         public InvalidParametersException(string message) : base(message) {
         }
 
+        /// <summary>
+        /// Constructs a new InvalidParametersException with a descriptive message and inner exception.
+        /// </summary>
+        /// <param name="message">A message describing the Exception.</param>
+        /// <param name="inner">The inner Exception.</param>
+        /// <returns></returns>
         public InvalidParametersException(string message, Exception inner) : base(message, inner) {
         }
     }
