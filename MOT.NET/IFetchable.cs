065bcd19fbe22aab7d96c36c7144c0bac504e051
Lewis Hazell <lewis.hazell@hotmail.co.uk>
2020-02-02 11:57:26 +0000

    Documented IFetchable<T>.

--- MOT.NET/IFetchable.cs
+++ MOT.NET/IFetchable.cs
@@ -1,7 +1,15 @@
 using System.Collections.Generic;
 
 namespace MOT.NET {
+    /// <summary>
+    /// Provides a mechanism for fetching multiple objects asynchronously.
+    /// </summary>
+    /// <typeparam name="T">The type of object to fetch.</typeparam>
     public interface IFetchable<T> {
+        /// <summary>
+        /// Fetches multiple objects asynchronously.
+        /// </summary>
+        /// <returns>An IAsyncEnumerable to access and process the returned objects individually.</returns>
         IAsyncEnumerable<T> FetchAsync();
     }
 }
